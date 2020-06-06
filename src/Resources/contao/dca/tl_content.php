5d5b92c97a3d012c7dd618f6f9bd8a4a93a39f2d
Markus Milkereit <markus.milkereit@magmell.de>
2020-06-06 11:30:27 +0200

    update for handling youtube no-cookie and splash/autoplay

--- src/Resources/contao/dca/tl_content.php
+++ src/Resources/contao/dca/tl_content.php
@@ -1,3 +1,7 @@
 <?php
 
 $GLOBALS['TL_DCA']['tl_content']['palettes']['privacyPopupOpener'] = '{type_legend},type;{template_legend:hide},customTpl;{protected_legend:hide},protected;{expert_legend:hide},guests;{invisible_legend:hide},invisible,start,stop';
+
+// privacy fix - domain is always no-cookie, hardwired in ce_youtube.html5
+$GLOBALS['TL_DCA']['tl_content']['fields']['youtubeOptions']['options'] = array_diff($GLOBALS['TL_DCA']['tl_content']['fields']['youtubeOptions']['options'], ['youtube_nocookie']);
+
